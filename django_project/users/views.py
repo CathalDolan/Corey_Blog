d98f815afaf63283e6801fa305938daf0dc5c8fa
CathalDolan <cathal@gizagig.com>
2022-09-03 14:13:20 +0000

    Tut 6.3: Added email field to User Registration form

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            # Saves the form to the DB
+            form.save()
             username = form.cleaned_data.get('username')
             # The "success" tag is imported into base.html in the messages class
             # f string used to add the message text content in this case
             messages.success(request, f'Account created for {username}!')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
