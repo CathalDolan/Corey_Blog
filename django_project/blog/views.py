23c39e56cf5306b37dd41cd181fe81fabe574cc8
CathalDolan <cathal@gizagig.com>
2022-09-03 13:07:49 +0000

    Tutorial 5.2 Registered Post on app admin

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Dummy list of dictionaries
-posts = [
-    {
-        'author': 'CFD',
-        'title': 'Blog Post 1',
-        'content': 'Content for blog post 1',
-        'date_posted': 'September 1st 2022',
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Content for blog post 2',
-        'date_posted': 'September 2nd 2022',
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
