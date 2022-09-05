e9d3eedf960e8c986d9a7ae306b1d93ed2705370
CathalDolan <cathal@gizagig.com>
2022-09-05 15:25:45 +0000

    Tut 10.3 9:30: View and url path created for viewing a specific post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'blog/home.html', context)
+# This is the original Home view. Replaced by PostListView class view
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#     }
+#     return render(request, 'blog/home.html', context)
 
 
+# This is the blog home view, created by adding variables
+# the corresponding url path also had to be updated
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'  # Convention: <app name>/<model name>_<viewtype>.html
     context_object_name = 'posts'
+    # Puts the list of blogs in order. Adding the - sign, displays newest to oldest (as opposed to the default which is the opposite)
     ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About Title'})
-
-
-
