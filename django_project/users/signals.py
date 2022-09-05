ce9a48dbde663e53ef6d55c347f26b5387de0b26
CathalDolan <cathal@gizagig.com>
2022-09-05 10:37:06 +0000

    Tut 8.3 26:20: Created a signal for creating a new User

---
+++ django_project/users/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+# The User Model is the Sender
+from django.contrib.auth.models import User
+# This is the Receiver
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
