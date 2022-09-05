4b4a2da0b1b9a598fdf1c9736eb8b77f19ad85e7
CathalDolan <cathal@gizagig.com>
2022-09-05 12:57:15 +0000

    Tut 9.4 16:30: Functionality added to Users Profile model that resizes uploaded images to make them more manageable for storing

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # This method runs after the model is saved
+    # It reduces the size of an uploaded image
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.wodth > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
