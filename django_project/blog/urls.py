5054f1527d172a03e27bad608090bb99a9120481
CathalDolan <cathal@gizagig.com>
2022-09-05 15:44:50 +0000

    (Tut 10.4 17:22: Links added to homepage blog list so that clicking on a blog title opens that individual post

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # path('', views.home, name='blog-home'), replaced by PostListView below.
     # .as_view converts it into a view
     path('', PostListView.as_view(), name='blog-home'),
+    # into before pk indicates that the data must be an integer
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
