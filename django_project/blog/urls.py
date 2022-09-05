50cb9033fdf602d8f2f18ee36906f681c85092b0
CathalDolan <cathal@gizagig.com>
2022-09-05 17:12:25 +0000

    Tut 10.4 19:20: Create a Post functionality

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView
+)
 from . import views
 
 urlpatterns = [
@@ -8,5 +12,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     # into before pk indicates that the data must be an integer
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 
 
@@ -23,6 +27,18 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # This method overrides default form valid method so that we 
+    # can add the author before the form is submitted.
+    def form_valid(self, form):
+        # Before the form is submitted get the name of the User
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 def about(request):
